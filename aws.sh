e7274236af25c9a69a25a4440dbdeebf47a9ba3a
Derek Boytim <derek@path-robotics.com>
2019-07-26 12:53:46 -0400

    Cusomizations

--- aws.sh
+++ aws.sh
@@ -3,13 +3,13 @@ DIR=$(dirname $0)
 cd $DIR
 
 echo "Recreating aws-secrets"
-kubectl delete -n infrastructure -f templates/aws-secrets.yml || true
-kubectl create -n infrastructure -f templates/aws-secrets.yml
+kubectl delete -n ci -f templates/aws-secrets.yml || true
+kubectl create -n ci -f templates/aws-secrets.yml
 
 echo "Recreating aws-role"
-kubectl delete -n infrastructure -f templates/aws-role.yml || true
-kubectl create -n infrastructure -f templates/aws-role.yml
+kubectl delete -n ci -f templates/aws-role.yml || true
+kubectl create -n ci -f templates/aws-role.yml
 
-echo "Recreating infrastructure aws-ecr-cron"
-kubectl delete -n infrastructure -f templates/ecr-cron.yml || true
-kubectl create -n infrastructure -f templates/ecr-cron.yml
+echo "Recreating ci aws-ecr-cron"
+kubectl delete -n ci -f templates/ecr-cron.yml || true
+kubectl create -n ci -f templates/ecr-cron.yml
